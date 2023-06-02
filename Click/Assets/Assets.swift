bbff1cd67faeb4627c293f830fc0a8e5e8c55a11
dianaoros <dianaorosb@gmail.com>
2023-06-02 10:13:51 -0700

    Add RSwift, gitignore & tabs assets

---
+++ Click/Assets/Assets.swift
@@ -0,0 +1,15 @@
+//
+//  Assets.swift
+//  Click
+//
+//  Created by Diana Oros on 6/2/23.
+//  Copyright © 2023 Click. All rights reserved.
+//
+
+import Rswift
+
+public typealias Asset = ImageResource
+
+extension Asset {
+    
+}
